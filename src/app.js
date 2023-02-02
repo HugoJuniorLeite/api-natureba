0077e26b130a544e642a469c6e1fdef69da4b7c4
Hugo Junior <hjunioreng@gmail.com>
2023-02-02 19:28:44 -0300

    chore: adjust code for deploy mongodb

--- src/app.js
+++ src/app.js
@@ -13,8 +13,5 @@ app.use(express.json());
 
 app.use([authRouter,/*naturebaRouter,*/productsRouter,buyRouter])
 
-
-
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
